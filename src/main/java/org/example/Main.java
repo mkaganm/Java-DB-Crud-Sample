57f202bc695ac52e317091ee131f136854449dd7
kagan.meric <kagan.meric@enuygun.com>
2023-03-26 20:54:29 +0300

    update pom.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,22 @@
 package org.example;
 
+import com.github.javafaker.Faker;
 import dbconn.DBUsers;
+import models.User;
 
 public class Main {
     public static void main(String[] args) {
-        DBUsers.connectDB();
-        DBUsers.closeDB();
+
+        DBUsers dbu = new DBUsers();
+
+        User user = new User();
+
+        user.setFakeUser();
+
+        dbu.addUserToDB(user);
+
+        for (User user1 : dbu.getUserList()) {
+            System.out.println(user1);
+        }
     }
 }
